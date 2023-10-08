d06d3714f0c59c135daddf4ca9a57ff28ad40913
meghalakhwan <meghalakhwan9@gmail.com>
2023-10-08 20:24:43 +0530

    final commit

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const passwordBox = document.getElementById("password");
-const length = 12;
+const lenght = 12;
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
 const symbol = "@#$%^&*()_+~|}{[]></-=";
 
+const allChars = upperCase + lowerCase + number + symbol;
+
 function createPassword(){
     let password = "";
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password +=lowercase[Math.floor(Math.random() * lowerCase.length)];
+    password += lowercase[Math.floor(Math.random() * lowerCase.length)];
     password += number[Math.floor(Math.random() * number.length)];
     password += symbol[Math.floor(Math.random() * symbol.length)];
 
+    while(lenght > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
 }
 
